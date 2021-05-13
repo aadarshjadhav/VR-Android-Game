5612fd728979133ab5c4e52801002fc9f26e31e3
Aadarsh <84113924+aadarshjadhav@users.noreply.github.com>
2021-05-13 23:54:27 +0530

    Add files via upload

---
+++ DoorScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    private Animator Animation;
+    public bool Isopen=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animation = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Dooropen()
+    {
+        if (Isopen == false)
+        {
+            Animation.SetTrigger("Open");
+            Isopen = true;
+        }
+        else if (Isopen == true)
+        {
+            Animation.SetTrigger("Close");
+            Isopen = false;
+        }
+
+
+    }
+}
